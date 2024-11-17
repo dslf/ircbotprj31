3446e055e121c52b30b1910360970925b536e2a4
Denis Lafin <dslf@live.ru>
2024-11-17 15:38:02 +0300

    connection

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-#include <WinSock2.h>
+#include <winsock2.h>
+#include <stdlib.h>
+#include <ws2tcpip.h>
 
 int main(){
     WSADATA wsaData;
+    SOCKET sock;
+    struct sockaddr_in serverAddr;
+    const char *IRCServer = "104.152.54.53";
+    int IRCPort = 6667;
     int result;
 
     result = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -10,8 +16,31 @@ int main(){
         printf("WSAStartup failed: %d\n", result);
         return 1;
     }
+    printf("WSAStartup works.\n");
 
-    printf("works.\n");
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        printf("Error creating: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+    printf("Socket created.\n");
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(IRCPort);
+    serverAddr.sin_addr.s_addr = inet_addr(IRCServer); 
+
+    if (connect(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        printf("Connect failed: %d\n", WSAGetLastError());
+        closesocket(sock);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Connected to IRC server at %s:%d\n", IRCServer, IRCPort);
+
+    closesocket(sock);
     WSACleanup();
     
     return 0;
