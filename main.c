5a2370a9af6975ac843dc4dbd57db3ba42172524
Denis Lafin <dslf@live.ru>
2024-11-17 18:24:48 +0300

    working skeleton

--- main.c
+++ main.c
@@ -4,15 +4,18 @@
 #include <stdlib.h>
 #include <ws2tcpip.h>
 
+#define NICKNAME "tezdsttter"
+#define CHANNEL "#jkpa1234"
+
 int main(){
     WSADATA wsaData;
     SOCKET sock;
     struct sockaddr_in serverAddr;
-    const char *IRCServer = "104.152.54.53";
+    const char *IRCServer = "104.152.54.53"; /* DALnet: halcyon.il.us.dal.net */
     int IRCPort = 6667;
     int result;
     char send_buffer[512];
-
+  
     result = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (result != 0) {
         printf("WSAStartup failed: %d\n", result);
@@ -41,16 +44,32 @@ int main(){
     }
 
     printf("Connected to IRC server at %s:%d\n", IRCServer, IRCPort);
-    strcpy(send_buffer, "NICK tezdsttter");  
-    send(sock, send_buffer,strlen(send_buffer));
-    
-    strcpy(send_buffer, "USER tezdstttefr");  
-    /* strcat(send_buffer, ""); */
+    sprintf(send_buffer, "NICK %s\r\n", NICKNAME);
+    send(sock, send_buffer, strlen(send_buffer), 0);
 
-    send(sock, send_buffer,strlen(send_buffer));
+    sprintf(send_buffer, "USER %s 8 * :%s\r\n", NICKNAME, NICKNAME);
+    send(sock, send_buffer, strlen(send_buffer), 0);
+    
+    sprintf(send_buffer, "JOIN %s\r\n", CHANNEL);
+    send(sock, send_buffer, strlen(send_buffer), 0);
+    
+    while (1) {
+        memset(send_buffer, 0, sizeof(send_buffer));
+        int bytesReceived = recv(sock, send_buffer, sizeof(send_buffer) - 1, 0);
+        if (bytesReceived <= 0) {
+            break; 
+        }
+
+        send_buffer[bytesReceived] = '\0';
+        printf("%s", send_buffer);
+
+        if (strstr(send_buffer, "PING") != NULL) {
+            sprintf(send_buffer, "PONG %s\r\n", send_buffer + 5);
+            send(sock, send_buffer, strlen(send_buffer), 0);
+        }
+    }
 
     closesocket(sock);
-    WSACleanup();
-    
+    WSACleanup();  
     return 0;
 }
